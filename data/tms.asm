a330baf3df1b7a484eaabaff73f16d66e8466efb
lorissa-becker <lorissa.becker@gmail.com>
2020-07-24 21:52:11 -0400

    Updated TM list

--- data/tms.asm
+++ data/tms.asm
@@ -1,52 +1,52 @@
 	db MEGA_PUNCH   ; Safari Zone
-	db DRAGON_RAGE  ; Rocket Hideout, Celadon Dept
-	db SWORDS_DANCE ; Silph Co
-	db WING_ATTACK  ; Route 4
+	db MOONBLAST    ; Rocket Hideout, Celadon Dept
+	db DRAGONBREATH ; Silph Co
+	db STEEL_WING   ; Route 4
 	db MEGA_KICK    ; Celadon Dept
 	db TOXIC        ; Koga Prize
-	db HORN_DRILL   ; Rocket Hideout
-	db BODY_SLAM    ; SS Anne
-	db TAKE_DOWN    ; Silph Co, Celadon Dept
-	db DOUBLE_EDGE  ; Rocket Hideout
-	db WATER_GUN    ; Misty Prize
-	db BUBBLEBEAM   ; Mt Moon
+	db X_SCISSOR    ; Rocket Hideout
+	db MAGICAL_LEAF ; SS Anne
+	db FIRE_PUNCH   ; Silph Co, Celadon Dept
+	db SILVER_WIND  ; Rocket Hideout
+	db WATER_PULSE  ; Misty Prize
+	db POWDER_SNOW  ; Mt Moon
 	db ICE_BEAM     ; Celadon Dept Roof
-	db BLIZZARD     ; Pokemon Mansion
+	db SKY_UPPERCUT ; Pokemon Mansion
 	db HYPER_BEAM   ; Beach House
-	db TAIL_WHIP    ; Route 12
+	db BODY_SLAM    ; Route 12
 	db SUBMISSION   ; Celadon Dept
-	db COUNTER      ; Celadon Dept
-	db SEISMIC_TOSS ; Route 25
-	db SLASH        ; Route 15
-	db MEGA_DRAIN   ; Erika Prize
-	db SOLARBEAM    ; Pokemon Mansion
-	db THUNDERSHOCK ; Victory Road
+	db ANCIENTPOWER ; Celadon Dept
+	db DRAGON_DANCE ; Route 25
+	db MUD_SHOT     ; Route 15
+	db GIGA_DRAIN   ; Erika Prize
+	db MUDDY_WATER  ; Pokemon Mansion
+	db FLASH_CANNON ; Victory Road
 	db THUNDERBOLT  ; Lt Surge Prize
 	db THUNDER      ; Power Plant
 	db EARTHQUAKE   ; Silph Co
 	db FISSURE      ; Giovanni Prize
 	db DIG          ; Cerulean City
 	db PSYCHIC_M    ; Saffron City
-	db PSYBEAM      ; Route 9
-	db MIMIC        ; Saffron City
-	db DOUBLE_TEAM  ; Safari Zone, Celadon Dept
-	db REFLECT      ; Power Plant, Celadon Dept
-	db HEADBUTT     ; Mt Moon, Celadon Dept
-	db ROCK_THROW   ; Pokemon Lab
-	db BIDE         ; Brock Prize
-	db FLAMETHROWER ; Celadon Prize Exchange
-	db FIRE_BLAST   ; Blaine Prize
+	db SHADOW_PUNCH ; Route 9
+	db POWER_GEM    ; Saffron City
+	db ICE_PUNCH    ; Safari Zone, Celadon Dept
+	db THUNDERPUNCH ; Power Plant, Celadon Dept
+	db AERIAL_ACE   ; Mt Moon, Celadon Dept
+	db HYDRO_PUMP   ; Pokemon Lab
+	db ROCK_BLAST   ; Brock Prize
+	db FIRE_BLAST   ; Celadon Prize Exchange
+	db FLAMETHROWER ; Blaine Prize
 	db SWIFT        ; Route 12
-	db SKULL_BASH   ; Safari Zone
-	db GUST         ; Celadon Dept
+	db DRAGON_CLAW  ; Safari Zone
+	db SCALD        ; Celadon Dept
 	db DREAM_EATER  ; Viridian City
-	db LICK         ; Celadon City
-	db REST         ; SS Anne
-	db THUNDER_WAVE ; Route 24
-	db PSYWAVE      ; Sabrina Prize
-	db EXPLOSION    ; Victory Road
+	db SOLARBEAM    ; Celadon City
+	db IRON_HEAD    ; SS Anne
+	db ROCK_THROW   ; Route 24
+	db EXTRASENSORY ; Sabrina Prize
+	db BLIZZARD     ; Victory Road
 	db ROCK_SLIDE   ; Celadon Dept Roof
-	db AURORA_BEAM  ; Celadon Dept Roof
+	db TRI_ATTACK   ; Celadon Dept Roof
 	db FLASH        ; Route 2
 	db CUT          ; SS Anne
 	db FLY          ; Route 16
